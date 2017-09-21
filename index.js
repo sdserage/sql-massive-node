20ef3ea3435de99b3f24155e04da35934dd63052
Scott Serage <xttocsurge51@gmail.com>
2017-09-21 16:12:01 -0600

    Completed up to but not including Step 7, so far no concrete method to test if anything is correct.

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const massive = require("massive");
 require("dotenv").config();
+const pc = require('./products_controller');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 massive(process.env.CONNECTION_STRING).then(dbInstance=>app.set('db',dbInstance));
+
+app.get(`/api/products`,pc.getAll);
+app.get(`/api/products/:id`,pc.getOne);
+app.put(`/api/products/:id`,pc.update);
+app.post(`/api/products`,pc.create);
+app.delete(`/api/products/:id`,pc.delete);
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{console.log(`Server listening on port ${port}...`);});
