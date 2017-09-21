a79e5285330901aef0b8607b560ed13ddc9c4848
Scott Serage <xttocsurge51@gmail.com>
2017-09-21 16:12:33 -0600

    Completed up to but not including Step 7, so far no concrete method to test if anything is correct.

--- products_controller.js
+++ products_controller.js
@@ -1,13 +1,16 @@
 module.exports = {
   create(req,res,next){
     const dbInstance = req.app.get('db');
-    dbInstance().create_product()
+    const {name,description,price,imageurl} = req.body;
+
+    dbInstance().create_product([name, description, price, imageurl])
       .then(()=>res.status(200).send())
       .catch(()=>res.status(500).send());
   },
   getOne(req,res,next){
     const dbInstance = req.app.get('db');
-    dbInstance().read_product()
+    const {id} = req.params;
+    dbInstance().read_product([id])
       .then(product=>res.status(200).send(product))
       .catch(()=>res.status(500).send());
   },
@@ -19,13 +22,15 @@ module.exports = {
   },
   update(req,res,next){
     const dbInstance = req.app.get('db');
-    dbInstance().update_product()
+    const {params,query} = req;
+    dbInstance().update_product([params.id,query.desc])
       .then(()=>res.status(200).send())
       .catch(()=>res.status(500).send());
   },
   delete(req,res,next){
     const dbInstance = req.app.get('db');
-    dbInstance().delete_product()
+    const {id} = req.params;
+    dbInstance().delete_product([id])
       .then(()=>res.status(200).send())
       .catch(()=>res.status(500).send());
   }
